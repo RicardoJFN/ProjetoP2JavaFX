d7696699f7b0ad80829f63f0c8f8c9ce2f2cfa02
Ricardo Nunes <ricardo.nunes.isla@gmail.com>
2015-11-13 09:50:34 +0000

    Alterações à classe Cliente.
    Já alterados os atributos para Propertys

--- src/p2/projeto/mlt/model/Cliente.java
+++ src/p2/projeto/mlt/model/Cliente.java
@@ -5,6 +5,11 @@
  */
 package p2.projeto.mlt.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  *
  * @author ricardon
@@ -16,14 +21,14 @@ public class Cliente {
      * Definição de atributos da classe Cliente
      */
     
-    protected String nome;
-    protected String morada;
-    protected int codigoPostal;
-    protected String localidade;
-    protected String pais;
-    protected int contactoTelefone;
-    protected int contactoTelemovel;
-    protected int contactoEmail;
+    protected StringProperty nome;
+    protected StringProperty morada;
+    protected IntegerProperty codigoPostal;
+    protected StringProperty localidade;
+    protected StringProperty pais;
+    protected IntegerProperty contactoTelefone;
+    protected IntegerProperty contactoTelemovel;
+    protected IntegerProperty contactoEmail;
     
     /**
      * Construtor da classe Cliente FALTA ACABAR
@@ -34,14 +39,14 @@ public class Cliente {
     public Cliente(String nome, String morada, int codigoPostal, String localidade, String pais, int contactoTelefone,
             int contactoTelemovel, int contactoEmail){
         
-        this.nome = nome;
-        this.morada = morada;
-        this.codigoPostal = codigoPostal;
-        this.localidade = localidade;
-        this.pais = pais;
-        this.contactoTelefone = contactoTelefone;
-        this.contactoTelemovel = contactoTelemovel;
-        this.contactoEmail = contactoEmail;
+        this.nome = new SimpleStringProperty(nome);
+        this.morada = new SimpleStringProperty(morada);
+        this.codigoPostal = new SimpleIntegerProperty(codigoPostal);
+        this.localidade = new SimpleStringProperty(localidade);
+        this.pais = new SimpleStringProperty(pais);
+        this.contactoTelefone = new SimpleIntegerProperty(contactoTelefone);
+        this.contactoTelemovel = new SimpleIntegerProperty(contactoTelemovel);
+        this.contactoEmail = new SimpleIntegerProperty(contactoEmail);
     }
     
 }
