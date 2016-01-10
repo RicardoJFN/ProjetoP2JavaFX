97f7fe5d31740d314fa409c53287f7a6073d9308
carlosb <carlosb@TDGI-CARLOSB.LAGOAS.PARK>
2016-01-10 20:41:57 +0000

    Ultima alteração controladorConsultarLevantamentos e classe levantamento

--- src/p2/projeto/mlt/controller/ControladorConsultarLevantamentos.java
+++ src/p2/projeto/mlt/controller/ControladorConsultarLevantamentos.java
@@ -21,37 +21,41 @@ import p2.projeto.mlt.model.Levantamento;
  * @author carlosb
  */
 public class ControladorConsultarLevantamentos {
-
-    @FXML
-    private javafx.scene.control.Button sair;
-    @FXML
-    private TableView<Levantamento> tabelaLevantamento;
-    @FXML
-    private TableColumn<Levantamento, String> numero;
-    @FXML
-    private TableColumn<Levantamento, String> data;
-
-    private ObservableList<Levantamento> levantamentos = FXCollections.observableArrayList();
-
-    public ObservableList<Levantamento> getDataLevantamento() {
-        return levantamentos;
-    }
-
-    @FXML
-    private void initialize() {
-        try {
-            // tabelaLevantamento.setEditable(true);
-            levantamentos = FXCollections.observableArrayList(Levantamento.selecionaLevantamento());
-            numero.setCellValueFactory(cellData -> cellData.getValue().getNumLevantamento().asString());
-            data.setCellValueFactory(cellData -> cellData.getValue().getDataLevantamento().asString());
-            //tabelaLevantamento.setItems(levantamentos); 
-        } catch (Exception e) {
-            e.printStackTrace();
+    
+        /**
+         * Definicao de atributos do controller Consultar Levantamentos
+         */
+    
+        @FXML
+        private javafx.scene.control.Button sair;
+        @FXML
+        private TableView<Levantamento> tabelaLevantamento;
+        @FXML
+        private TableColumn<Levantamento, Double> numero;
+        @FXML
+        private TableColumn<Levantamento, Integer> data;
+       
+        
+        private ObservableList<Levantamento> levantamentos = FXCollections.observableArrayList();
+        
+        
+        public ObservableList<Levantamento> getDataLevantamento(){
+            return levantamentos;
         }
-    }
-
-    @FXML
-    public void voltaMenuPrincipal() {
+        
+        
+        @FXML
+        private void initialize(){
+            tabelaLevantamento.setEditable(true);
+            //numero.setCellValueFactory(cellData -> cellData.getValue().getNumLevantamento());
+        }
+        
+        /**
+        * Metodo para botao Voltar que vai permitir voltar a janela anterior
+        */
+        
+        @FXML
+        public void voltaMenuPrincipal() {
         try {
             Stage consultarLevantamentos = (Stage) sair.getScene().getWindow();
             consultarLevantamentos.close();
@@ -64,27 +68,27 @@ public class ControladorConsultarLevantamentos {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+       }
+        
+        /**
+        * Metodo para botao ver levantamento presente na janela Consultarlevantamentos -
+        * botao para view resumoLevantamento que vai permitir ver o
+        * resumo do levantamento seleccionado
+        */
+        
+        @FXML
+        private void mostraResumoLevantamento() {
 
-    /**
-     * Metodo para botao ver levantamento presente na janela
-     * Consultarlevantamentos - botao para view resumoLevantamento que vai
-     * permitir ver o resumo do levantamento seleccionado
-     */
-    @FXML
-    private void mostraResumoLevantamento() {
-
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/p2/projeto/mlt/view/ResumoLevantamento.fxml"));
-            Parent root = (Parent) fxmlLoader.load();
-            Stage stage = new Stage();
-            stage.setTitle("Resumo levantamento");
-            stage.setScene(new Scene(root));
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/p2/projeto/mlt/view/ResumoLevantamento.fxml"));
+                Parent root = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.setTitle("Resumo levantamento");
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (Exception e) {
         }
 
     }
-
+    
 }
